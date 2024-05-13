fab462b6b68d1dadaa8648e9197a574bb9ce5917
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-05-13 22:01:23 +0530

    Just made button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
 
 const App = () => {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>;
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Button className="mt-10" href="#login"></Button>
+        <Button className="mt-10" href="#login">
+          Something
+        </Button>
       </div>
       <ButtonGradient />
     </>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import ButtonSvg from "../assets/svg/ButtonSvg";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ className, href, onClick, children, px, white }) => {
+  const classes = `button relative`;
+
+  const renderButton = () => (
+    <button className={classes}>
+      <span>{children}</span>
+      {ButtonSvg(white)}
+    </button>
+  );
+
+  return renderButton();
 };
 
 export default Button;
