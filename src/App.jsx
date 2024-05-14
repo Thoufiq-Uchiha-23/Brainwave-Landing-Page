bcf52da5b7f3adc69ef61f2321ec7b77216c9a0f
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-05-14 22:24:40 +0530

    Made Minor changes to fix errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         {/* <Button className="mt-10">Something</Button> */}
         <Header />
+        <Hero />
       </div>
       <ButtonGradient />
     </>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -15,7 +15,7 @@ const Button = ({ className, href, onClick, children, px, white }) => {
 
   const renderLink = () => (
     <a href={href} className={classes}>
-      <span>{children}</span>
+      <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </a>
   );
