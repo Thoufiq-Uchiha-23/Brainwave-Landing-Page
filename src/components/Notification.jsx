45f929fe256e766b5bf4fd499ff3876716ecb8c2
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-05-15 22:21:48 +0530

    Creating 2-3 floating boxes around the AI Image

---
+++ src/components/Notification.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { notificationImages } from "../constants";
+
+const Notification = ({ className, title }) => {
+  return (
+    <div
+      className={`${
+        className || ""
+      } flex items-center p-4 pr-6 bg-n-9/40 backdrop-blur border-n-1/10 rounded-2xl gap-5`}
+    >
+      <div className="flex-1">
+        <h6 className="mb-1 font-semibold justify-between text-base">
+          {title}
+        </h6>
+
+        <div className="flex items-center justify-between">
+          <ul className="flex -m-0.5">
+            {notificationImages.map((item, index) => (
+              <li
+                key={index}
+                className="flex w-6 h-6 border-2 border-n-12 rounded-full overflow-hidden"
+              >
+                <img
+                  src={item}
+                  className="w-full"
+                  width={20}
+                  height={20}
+                  alt={item}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Notification;
