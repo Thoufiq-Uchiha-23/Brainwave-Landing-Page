50ba97d6e72a79b262b48f73b74372f4f911acec
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-05-16 22:03:03 +0530

    Mission:collaboration Section Completed

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -17,7 +17,7 @@ const Benefits = () => {
         <div className="flex flex-wrap gap-10 mb-10">
           {benefits.map((item) => (
             <div
-              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+              className="block relative p-0.5 bg-no-repeat border border-1 border-yellow-300 bg-[length:100%_100%] md:max-w-[24rem]"
               style={{
                 backgroundImage: `url(${item.backgroundUrl})`,
               }}
