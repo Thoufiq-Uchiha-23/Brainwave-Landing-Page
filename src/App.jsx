c53690cef8ba2d267c1de79bea82161cf1b01813
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-05-17 21:53:22 +0530

    Mission: Pricing Section also Done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Button from "./components/Button";
 import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
 import Services from "./components/Services";
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
         <Benefits />
         <Collaboration />
         <Services />
+        <Pricing />
       </div>
       <ButtonGradient />
     </>
---
+++ src/components/PricingList.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { pricing } from "../constants";
+import Button from "./Button";
+import { check } from "../assets";
+
+const PricingList = () => {
+  return (
+    <div className="flex gap-[1rem] max-lg:flex-wrap">
+      {pricing.map((item) => (
+        <div
+          id={item.id}
+          className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8 odd:my-4 [&>h4]:first:text-color-2 [&>h4]:even:text-color-1 [&>h4]:last:text-color-3"
+        >
+          <h4 className="h4 mb-4">{item.title}</h4>
+
+          <p className="body-2 min-h-[4rem] mb-3 text-n1/50  ">
+            {item.description}
+          </p>
+
+          <div className="flex items-center h-[5.5rem]">
+            {item.price && (
+              <>
+                <div className="h3">$</div>
+                <div className="text-[5.5rem] leading-none font-bold ">
+                  {item.price}
+                </div>
+              </>
+            )}
+          </div>
+
+          <Button
+            className="w-full mb-6"
+            href={item.price ? "/pricing" : "mailto:contact@jsmastery.pro"}
+            white={!!item.price}
+          >
+            {item.price ? "Get started" : "Contact Us"}
+          </Button>
+
+          <ul>
+            {item.features.map((feature, index) => (
+              <li className="flex items-start py-5 border-t border-n-6">
+                <img src={check} width={24} height={24} alt="Check" />
+                <p className="body-2 ml-4">{feature}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PricingList;
