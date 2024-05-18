a4d59f6eba984b9c286c5969b32b4bc2a4f23197
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-05-18 19:23:10 +0530

    Just added id proper to link to Pricing section

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -7,7 +7,7 @@ import { LeftLine, RightLine } from "./design/Pricing";
 
 const Pricing = () => {
   return (
-    <Section className="overflow-hidden">
+    <Section className="overflow-hidden" id="pricing">
       <div className="container relative z-2">
         <div className="hidden relative justify-center mb-[6.5rem] lg:flex">
           <img
