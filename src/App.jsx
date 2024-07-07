41fe0b050f8b905a11068c1491db62c1e5931329
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-07-07 22:18:17 +0530

    added sign up page

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,40 @@ import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
 import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
+// Firebase
+import { app } from "./firebase";
+import { useState, useEffect } from "react";
+import Signup from "./pages/Signup";
+import SigninPage from "./pages/Signin";
+import { onAuthStateChanged, signOut, getAuth } from "firebase/auth";
+
+const auth = getAuth(app);
 
 const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // Yes, you are logged in
+        setUser(user);
+      } else {
+        // User is logged out
+        console.log("You are logged out");
+        setUser(null);
+      }
+    });
+  }, []);
+
+  if (user === null) {
+    return (
+      <div className="App">
+        <Signup />
+        {/* <SigninPage /> */}
+      </div>
+    );
+  }
+
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
